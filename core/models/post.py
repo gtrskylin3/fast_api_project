ce51bfd2c5ea767aa824cf5ed519f35a1f75b9a3
daniil <wwcern0vdan11lzxc@example.com>
2025-08-05 11:49:36 +0700

    create user relation mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,15 +1,31 @@
-from typing import TYPE_CHECKING
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Text, ForeignKey
 
 from .base import Base
-
-if TYPE_CHECKING:
-    from .user import User
+from .mixins import UserRelationMixin
 
     # если идет проверка типов а не выполнение кода и избегаем цикл импорт
 
-class Post(Base):
+# class Post(Base):
+#     title: Mapped[str] = mapped_column(String(100), unique=False)
+#     body: Mapped[str] = mapped_column(
+#         Text,
+#         default='',
+#         server_default=''
+#     )
+
+#     user_id: Mapped[int] = mapped_column(
+#         ForeignKey("users.id"),
+#         # nullable=False
+#     )
+#     # user: Mapped[User] # неможем будет цикл импорт 
+#     user: Mapped["User"] = relationship(back_populates="posts")
+
+class Post(Base, UserRelationMixin):
+    # _user_id_nullable = False
+    # _user_id_unique = False
+    _user_back_populates = 'posts'
+
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
         Text,
@@ -17,9 +33,4 @@ class Post(Base):
         server_default=''
     )
 
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-        # nullable=False
-    )
-    # user: Mapped[User] # неможем будет цикл импорт 
-    user: Mapped["User"] = relationship(back_populates="posts")
+   
